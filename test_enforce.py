dd14346baea870516a905318acba1df668145ce8
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-04-28 18:35:29 +0300

    Add test_nok15()

--- test_enforce.py
+++ test_enforce.py
@@ -9,9 +9,16 @@ def test_ok10():
     pc = StockItem("Computer", "EAA5000", "Hardware", 599, 3)
     assert pc.name == "Computer" and pc.category == "Hardware"
     assert pc.productid == "EAA5000"
+    assert pc.price == 599 and pc.quantity == 3 and pc.value == 1797
 
 
 def test_nok10():
     with pytest.raises(ValueError) as e:
         StockItem("", "ABC1000", "Software", 129, 2)
     assert str(e.value) == "name may not be empty"
+
+
+def test_nok15():
+    with pytest.raises(ValueError) as e:
+        StockItem("Printer", "KXV5500", "Vaporware", 129, 2)
+    assert str(e.value) == "category cannot be set to Vaporware"
