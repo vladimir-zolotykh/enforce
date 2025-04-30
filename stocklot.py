9e9f9d6965c16ec3b76dcc9e244ff7c64de5b29c
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-04-30 18:42:18 +0300

    Initial import
    Convert StockItem into StockLot , descriptors vs decorators

---
+++ stocklot.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import numbers
+import re
+from validator import StringValidated, NumberValidated
+
+
+class StockLot:
+    name = StringValidated()
+    productid = StringValidated(regex=r"^[A-Z]{3}\d{4}$")
+    category = StringValidated(
+        oneof=frozenset(["Consumables", "Hardware", "Software", "Media"])
+    )
+    price = NumberValidated(1, 1e06)
+    quantity = NumberValidated(1, 1000)
+
+    def __init__(self, name, productid, category, price, quantity):
+        self.name = name
+        self.productid = productid
+        self.category = category
+        self.price = price
+        self.quantity = quantity
+
+    @property
+    def value(self):
+        return self.price * self.quantity
+
+
+if __name__ == "__main__":
+    pc = StockLot("Computer", "EAA5000", "Hardware", 599, 3)
+    # pc = StockLot("Computer", 0x123, "Hardware", 599, 3)
+    print(f"{vars(pc) = }")
+    # assert pc.name == "Computer" and pc.category == "Hardware"
+    # assert pc.productid == "EAA5000"
+    # assert pc.price == 599 and pc.quantity == 3 and pc.value == 1797
