75bbfc62a4b5230e9de5de8d897c352a931e1b56
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-01 08:41:17 +0300

    Add test_nok10, 15

--- test_validator.py
+++ test_validator.py
@@ -10,3 +10,16 @@ def test_ok10():
     assert pc.name == "Computer" and pc.category == "Hardware"
     assert pc.productid == "EAA5000"
     assert pc.price == 599 and pc.quantity == 3 and pc.value == 1797
+
+
+def test_nok10():
+    with pytest.raises(ValueError) as e:
+        StockLot("", "ABC1000", "Software", 129, 2)
+    assert str(e.value) == "'name' is empty"
+
+
+def test_nok15():
+    with pytest.raises(ValueError) as e:
+        StockLot("Printer", "KXV5500", "Vaporware", 129, 2)
+    consumables = frozenset(["Consumables", "Hardware", "Media", "Software"])
+    assert str(e.value) == f"'Vaporware' isn't in {consumables}"
